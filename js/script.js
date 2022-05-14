301902b0234c6ab6280c990727926578f2956e14
leska-ver <lyaisan.333@yandex.ru>
2022-05-14 18:26:17 +0500

    aura-Max

--- js/script.js
+++ js/script.js
@@ -1,19 +1,53 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     
-  //Слайдер
-  const swiper = new Swiper('.new__slider', {
+  //Для одного Слайдера
+  // const swiper = new Swiper('.new__slider', {
+  //   loop: true,
+  //   spaceBetween: 20,
+  //   slidesPerView: 1,
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //     renderBullet: function (index, className) {
+  //       return '<span class="' + className + '">' + (index + 1) + "</span>";
+  //     },
+  //   },
+  // });
+
+
+  //Для двух Слайдеров
+  //Слайдер-1
+  const swiper1 = new Swiper('.new__slider_none-777', {
     loop: true,
     spaceBetween: 20,
     slidesPerView: 1,
     pagination: {
-      el: ".swiper-pagination",
+      el: ".swiper-pagination-1",
       clickable: true,
       renderBullet: function (index, className) {
         return '<span class="' + className + '">' + (index + 1) + "</span>";
       },
     },
-  });
+  }); 
+  
+  //Слайдер-2
+    const swiper2 = new Swiper('.new__slider_clone-777', {
+      loop: true,
+      spaceBetween: 20,
+      slidesPerView: 1,
+      pagination: {
+        el: ".swiper-pagination-2",
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+      },
+    }); 
+
+
+
+
 
 
 
